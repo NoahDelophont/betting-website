873f21cd02e62aff0ece4fad01e64a1e14ed25cf
Noah <delophont.noah@outlook.fr>
2018-10-10 16:06:15 +0200

    FOSUserBundle

--- betnplay/config/bundles.php
+++ betnplay/config/bundles.php
@@ -4,4 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    FOS\UserBundle\FOSUserBundle::class => ['all' => true],
 ];

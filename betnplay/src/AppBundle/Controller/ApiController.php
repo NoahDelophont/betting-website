1648f7312828df67c40c1c721bfb75a823b4ca95
Noah <delophont.noah@outlook.fr>
2018-11-16 17:18:50 +0100

    Update ApiController.php

--- betnplay/src/AppBundle/Controller/ApiController.php
+++ betnplay/src/AppBundle/Controller/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Game;
 use AppBundle\Entity\LastUpdated;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,24 +20,52 @@ class ApiController extends Controller
             $this->container->get('profiler')->disable();
         }
 
+        $upToDate = true;
+        $date = date('d/m/y');
         $bdd_lastUpdated = $this->getDoctrine()->getRepository('AppBundle:LastUpdated');
         $lastUpdate = $bdd_lastUpdated->findOneBy(array('data'=>'game')) ;
         if($lastUpdate!=NULL) {
-            $date = $lastUpdate->getUtcDate();
+            $up_date = $lastUpdate->getUtcDate();
+            if($up_date!=$date) {
+                $upToDate = false;
+            }
         } else {
-            $date = date('d/m/y');
             $entity = new LastUpdated();
             $entity->setData('game');
             $entity->setUtcDate($date);
         }
 
-        $uri = 'http://api.football-data.org/v2/competitions/2015/matches?status=FINISHED';
-        $reqPrefs['http']['method'] = 'GET';
-        $reqPrefs['http']['header'] = 'X-Auth-Token: 839c5a615c954184bf1a858a5f49005e';
-        $stream_context = stream_context_create($reqPrefs);
-        $response = file_get_contents($uri, false, $stream_context);
-        $matches = json_decode($response, true);
-        //var_dump($matches);
+        $bdd_game = $this->getDoctrine()->getRepository('AppBundle:Game');
+        if(!$upToDate) {
+            $uri = 'http://api.football-data.org/v2/competitions/2015/matches?status=FINISHED';
+            $reqPrefs['http']['method'] = 'GET';
+            $reqPrefs['http']['header'] = 'X-Auth-Token: 839c5a615c954184bf1a858a5f49005e';
+            $stream_context = stream_context_create($reqPrefs);
+            $response = file_get_contents($uri, false, $stream_context);
+            $matches = json_decode($response, true);
+
+            for ($i = 0;$i<count($matches['match']);$i++) {
+                $result = $bdd_game->findOneBy(array('api_id'=>$matches['match'][$i]['id']));
+                if($result==NULL) {
+                    $match = new Game();
+                    $match->setApiId($matches['match'][$i]['id']);
+                    $match->setUtcDate($matches['match'][$i]['utcDate']);
+                    $match->setAwayTeam(json_encode($matches['match'][$i]['awayTeam']));
+                    $match->setCompetition(json_encode($matches['match'][$i]['competition']));
+                    $match->setCote('{1.4,1.5,1.3}');
+                    $match->setHomeTeam(json_encode($matches['match'][$i]['homeTeam']));
+                    $match->setMatchDay($matches['match'][$i]['matchDay']);
+                    $match->setScore(json_encode($matches['match'][$i]['score']));
+
+                    $bdd_game->persistent($match);
+                }
+            }
+            $bdd_game->flush();
+
+
+        } else {
+
+        }
 
         return $this->render(
             'home/home.html.twig',

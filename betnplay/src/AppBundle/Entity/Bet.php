276c737148e058aff969550e965e64296d9159ff
Noah <delophont.noah@outlook.fr>
2018-11-18 01:42:33 +0100

    AJout le la bdd qui retient les paris

---
+++ betnplay/src/AppBundle/Entity/Bet.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Bet
+ *
+ * @ORM\Table(name="bet")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\BetRepository")
+ */
+class Bet
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_user", type="integer")
+     */
+    private $idUser;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_game", type="integer")
+     */
+    private $idGame;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="team", type="integer")
+     */
+    private $team;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set idUser
+     *
+     * @param integer $idUser
+     *
+     * @return Bet
+     */
+    public function setIdUser($idUser)
+    {
+        $this->idUser = $idUser;
+
+        return $this;
+    }
+
+    /**
+     * Get idUser
+     *
+     * @return int
+     */
+    public function getIdUser()
+    {
+        return $this->idUser;
+    }
+
+    /**
+     * Set idGame
+     *
+     * @param integer $idGame
+     *
+     * @return Bet
+     */
+    public function setIdGame($idGame)
+    {
+        $this->idGame = $idGame;
+
+        return $this;
+    }
+
+    /**
+     * Get idGame
+     *
+     * @return int
+     */
+    public function getIdGame()
+    {
+        return $this->idGame;
+    }
+
+    /**
+     * Set team
+     *
+     * @param integer $team
+     *
+     * @return Bet
+     */
+    public function setTeam($team)
+    {
+        $this->team = $team;
+
+        return $this;
+    }
+
+    /**
+     * Get team
+     *
+     * @return int
+     */
+    public function getTeam()
+    {
+        return $this->team;
+    }
+}
+
